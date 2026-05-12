0e6d62b27b7f6474d79998410eb3a295c994c11b
Pawan Kumar <melomaniacpawan@gmail.com>
2026-05-12 14:13:34 +0545

    Remove Manifest in Storages

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -103,11 +103,11 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # =========================
 # PASSWORD VALIDATION
